537f17c204de35b29c55e3b2912796e7c73a9d86
松岡侑幹 <yuki@yukinoMacBook-Air.local>
2024-02-05 16:16:15 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Amplify } from 'aws-amplify';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import config from './amplifyconfiguration.json';
+import Header from './components/Footer';
+import ImageUpload from './components/ImageUpload';
+import TemplateDisplay from './components/TemplateDisplay';
+import './App.css'; // 確認してください、これがApp.cssファイルをインポートしています。
 
-function App() {
+Amplify.configure(config);
+
+function App({ signOut, user }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Router>
+        <Header />
+        <div className="header-container">
+          <h1>Hello {user.username}</h1>
+          <button className="sign-out-button" onClick={signOut}>Sign out</button>
+        </div>
+        <Routes>
+          <Route path="/" element={<ImageUpload />} />
+          <Route path="/template-display" element={<TemplateDisplay userName={user.username} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
